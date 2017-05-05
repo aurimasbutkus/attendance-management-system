325edff482c3a9bdf6c350595430119cdf8dab89
Marius Eismantas <gmmarius@gmail.com>
2017-05-05 18:09:18 +0300

    Added roles

---
+++ project_managment_system/src/main/java/com/ems/userinfo/Role.java
@@ -0,0 +1,35 @@
+package com.ems.userinfo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="username")
+    private String username;
+    @Column(name="role")
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
