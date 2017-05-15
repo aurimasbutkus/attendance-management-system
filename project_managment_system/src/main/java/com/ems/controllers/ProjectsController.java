9ef1991a3174779daa90439d5221b2fa875ab3f4
Marius Eismantas <gmmarius@gmail.com>
2017-05-15 20:26:39 +0300

    Changed project link

--- project_managment_system/src/main/java/com/ems/controllers/ProjectsController.java
+++ project_managment_system/src/main/java/com/ems/controllers/ProjectsController.java
@@ -37,39 +37,34 @@ public class ProjectsController {
     @Autowired
     private MessageSource messageSource;
 
-    @RequestMapping(value="projects", method = RequestMethod.GET)
-    public String projects( Model model, Authentication authentication){
+    @RequestMapping(value="project/{id}", method = RequestMethod.GET)
+    public String getProject(@PathVariable("id") int id, Model model, Authentication authentication){
         String username = authentication.getName();
         Integer user_id = userService.getUser(username).getId();
         model.addAttribute("projects", projectService.listAllUserProjects(user_id));
-        return "projects";
-    }
-
-    @RequestMapping(value="project-core/{id}", method = RequestMethod.GET)
-    public String getProject(@PathVariable("id") int id, Model model, Authentication authentication){
         model.addAttribute("currentUserName", authentication.getName());
         model.addAttribute("project", projectService.getProject(id));
         model.addAttribute("tasks", projectService.listAllProjectTasks(id));
         model.addAttribute("subtasks", projectService.listAllSubtasks());
         model.addAttribute("newTask",new Task());
         model.addAttribute("newSubtask",new Subtask());
-        return "project-core";
+        return "project";
     }
 
-    @PostMapping(value="project-core/{id}/add-task")
+    @PostMapping(value="project/{id}/add-task")
     public String addTask(@ModelAttribute("newTask") Task newTask, BindingResult bindingResult,
                           @PathVariable("id") Integer id, Model model, Authentication authentication){
         newTask.setFkProject(id);
         newTask.setCreationDate(new Date(System.currentTimeMillis()));
         projectService.create(newTask);
-        return "redirect:/project-core/{id}";
+        return "redirect:/project/{id}";
     }
-    @RequestMapping(value="/project-core/{project_id}/remove-task/{task_id}", method = RequestMethod.POST)
-    public String removeTask(@PathVariable("task_id") Integer task_id) {
+    @RequestMapping(value="/project/{project_id}/remove-task/{task_id}", method = RequestMethod.POST)
+    public String removeTask(@PathVariable("project_id") Integer project_id, @PathVariable("task_id") Integer task_id) {
         projectService.deleteTask(task_id);
-        return "redirect:/project-core/{id}";
+        return "redirect:/project/{project_id}";
     }
-    @PostMapping(value="project-core/{project_id}/add-subtask/{task_id}")
+    @PostMapping(value="project/{project_id}/add-subtask/{task_id}")
     public String addTask(@ModelAttribute("newSubtask") Subtask newSubtask, BindingResult bindingResult,
                           @PathVariable("project_id") Integer project_id,
                           @PathVariable("task_id") Integer task_id, Model model,
@@ -77,15 +72,15 @@ public class ProjectsController {
         newSubtask.setFkTask(task_id);
         newSubtask.setStatus(2);
         projectService.create(newSubtask);
-        return "redirect:/project-core/{project_id}";
+        return "redirect:/project/{project_id}";
     }
 
-    @RequestMapping(value="project-core/{project_id}/remove-subtask/{subtask_id}", method = RequestMethod.GET)
+    @RequestMapping(value="project/{project_id}/remove-subtask/{subtask_id}", method = RequestMethod.GET)
     public String removeSubtask(@PathVariable("project_id") Integer project_id,
                                 @PathVariable("subtask_id") Integer subtask_id,Model model,
                                 Authentication authentication){
         projectService.deleteSubtask(subtask_id);
-        return "redirect:/project-core/{project_id}";
+        return "redirect:/project/{project_id}";
     }
 
     @GetMapping(value="projects/new")
